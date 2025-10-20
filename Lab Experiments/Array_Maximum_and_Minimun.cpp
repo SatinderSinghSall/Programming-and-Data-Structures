bb443caca964969b67be14da1e5b735ba1330a92
Satinder Singh Sall <satindersinghsall111@gmail.com>
2025-10-20 17:06:06 +0530

    Lab Exprements - Arrays.

--- Lab Experiments/Array_Maximum_and_Minimun.cpp
+++ Lab Experiments/Array_Maximum_and_Minimun.cpp
@@ -3,23 +3,38 @@
 #include <stdio.h>
 
 int main() {
-	printf("\nArray maximun and minimum value: \n");
+    printf("\nArray maximum and minimum value:\n");
 
-	int elementNum;
-	int myNumber[elementNum];
+    int elementNum;
+    printf("\nEnter number of array values: ");
+    scanf("%d", &elementNum);
 
-	printf("\nEnter number of array values: ");
-	scanf("%d", &elementNum);
+    int myNumber[elementNum];
 
-	printf("\nEnter %d array elements: ", elementNum);
-	for(int i = 0; i < elementNum; i ++) {
-		scanf("%d", &myNumber[i]);		
-	}
+    printf("\nEnter %d array elements: ", elementNum);
+    for(int i = 0; i < elementNum; i++) {
+        scanf("%d", &myNumber[i]);		
+    }
 
-	printf("\nArray Elements:");
-	for(int i = 0; i < elementNum; i ++) {
-		printf(" %d", myNumber[i]);
-	}
+    printf("\nArray Elements:");
+    for(int i = 0; i < elementNum; i++) {
+        printf(" %d", myNumber[i]);
+    }
+
+    int maxNum = myNumber[0];
+    int minNum = myNumber[0];
+
+    for(int i = 1; i < elementNum; i++) {
+        if(myNumber[i] > maxNum) {
+            maxNum = myNumber[i];
+        }
+        if(myNumber[i] < minNum) {
+            minNum = myNumber[i];
+        }
+    }
+
+    printf("\nMaximum Number: %d", maxNum);
+    printf("\nMinimum Number: %d\n", minNum);
 
     return 0;
 }
