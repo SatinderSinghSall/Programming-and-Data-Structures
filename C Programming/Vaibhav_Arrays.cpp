c6ae83340f9b4c2e8db2f8f1b795ca71217277b1
Satinder Singh Sall <satindersinghsall111@gmail.com>
2025-10-21 21:45:46 +0530

    DSA: Arrays.

---
+++ C Programming/Vaibhav_Arrays.cpp
@@ -0,0 +1,16 @@
+// Arrays in C:
+
+#include <stdio.h>
+
+int main() {
+	printf("\n\t Arrays in C: \n");
+
+	int crickrtJN[] = {20, 30, 1, 4, 40};
+
+	for(int i = 0; i < 5; i ++) {
+		printf("%d\n", crickrtJN[i]);
+	}
+
+	return 0;
+}
+
---
+++ Lab Experiments/Array_OddEven.cpp
@@ -0,0 +1,10 @@
+// Find odd and even numbers in an array:
+
+#include <stdio.h>
+
+int main() {
+    printf("\nArray odd & even numbers:\n");
+
+    return 0;
+}
+
