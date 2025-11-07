d50a47add657a8beb3b52ea4241a0ac41172aa18
Satinder Singh Sall <satindersinghsall111@gmail.com>
2025-11-07 20:16:09 +0530

    DSA: Arrays in Data Structure | Declaration, Initialization, Memory representation.

---
+++ C Programming/Array_DS.cpp
@@ -0,0 +1,22 @@
+// Array Data Structure:
+
+#include <stdio.h>
+
+int main() {
+	printf("\n===== Array Data Structure: =====\n");
+
+	int number[5];
+
+	printf("\nEnter array values of size 5 : ");
+	for(int i = 0; i < 5; i ++) {
+		scanf("%d", &number[i]);
+	}
+
+	printf("\nArray values are:\n");
+	for(int j = 0; j < 5; j ++) {
+		printf("Index %d value is %d\n", j, number[j]);
+	}
+
+	return 0;
+}
+
