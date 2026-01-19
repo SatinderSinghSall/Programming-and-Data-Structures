cc502886b8b1dfae9555035acfa7136563f91232
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-01-19 22:46:07 +0530

    DSA at KiiT: Recursion Algorithm.

--- DSA - Java/Data Structures and Algorithms/src/Recursion.java
+++ DSA - Java/Data Structures and Algorithms/src/Recursion.java
@@ -1,7 +1,34 @@
 // Recursion:
 
 public class Recursion {
+    public static int Factorial(int n) {
+        if (n == 1) { // base case
+            return 1;
+        }
+
+        return n * Factorial(n - 1); // recursive case
+    }
+
+    // Sum of natural nums:
+    public static int SumOfNumbers(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        return n + SumOfNumbers(n - 1);
+    }
+
+    public static int Factorial2(int n) {
+        if (n == 1) { // base case
+            return 1;
+        }
+
+        return n * Factorial2(n - 1); // recursive case
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        System.out.println(Factorial(5)); // 120
+        System.out.println(SumOfNumbers(5)); // 15
+        System.out.println(Factorial2(5)); // 120
     }
 }
